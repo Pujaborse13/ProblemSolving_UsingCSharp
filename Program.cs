e1979f002fc47993fb734de7a84c4b7bb92fbe2f
Puja Borse <pujaborse13@gmail.com>
2025-02-07 13:29:05 +0530

    Calculate number of workdays between two Days

--- Program.cs
+++ Program.cs
@@ -6,26 +6,37 @@ class Program
     static void Main()
     {
         //Console .WriteLine("Welcome to problem solving");
-            /*
+        /*
 
-            //Movie Ticket Price
-            MovieTicketPrice movieTicketPrice = new MovieTicketPrice();
-            movieTicketPrice.CalculateTicketPrice();
+        //Movie Ticket Price
+        MovieTicketPrice movieTicketPrice = new MovieTicketPrice();
+        movieTicketPrice.CalculateTicketPrice();
 
-            //Log In System
-            LogInSystem logInSystem = new LogInSystem();
-            logInSystem.LogIn();
+        //Log In System
+        LogInSystem logInSystem = new LogInSystem();
+        logInSystem.LogIn();
 
 
 
-            //Menu Driven Calculator
-            MenuDrivenCalculator menuDrivenCalculator = new MenuDrivenCalculator();
-            menuDrivenCalculator.Calculate();
-            */
+        //Menu Driven Calculator
+        MenuDrivenCalculator menuDrivenCalculator = new MenuDrivenCalculator();
+        menuDrivenCalculator.Calculate();
 
-            //Calculate Air Quality Index ()
-            CalculateAirQualityIndex calculator = new CalculateAirQualityIndex();
-            calculator.CalculateAQI();
+
+        //Calculate Air Quality Index 
+        CalculateAirQualityIndex calculator = new CalculateAirQualityIndex();
+        calculator.CalculateAQI();
+        
+
+        //Display Current Time In DifferentZone
+        CurrentTimeInDifferentZone currentTimeInDifferentZone = new CurrentTimeInDifferentZone();
+        currentTimeInDifferentZone.DisplayCurrentTimeInDifferentZone();
+
+        */
+
+        //Number of Workdays between two dates
+        NumberOfWorkdays numberOfWorkdays = new NumberOfWorkdays();
+        numberOfWorkdays.CalculateWorkDays();
 
 
 
