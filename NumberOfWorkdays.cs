b8a250ce589ea2ff3870583b8d32b4a7909b4ec0
Puja Borse <pujaborse13@gmail.com>
2025-02-07 13:31:22 +0530

    Calculate number of workdays between two Days

---
+++ NumberOfWorkdays.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolvingUsingCSharp
+{
+    //Write a program that calculates the number of workdays (Monday to Friday) between two dates.
+    internal class NumberOfWorkdays
+    {
+        int workDays = 0;
+        public void CalculateWorkDays()
+        { 
+           Console.WriteLine("Enter Start Date (yyyy-mm-dd): ");
+            DateTime startDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter End Date (yyyy-mm-dd): ");
+            DateTime endDate = DateTime.Parse(Console.ReadLine());
+
+            while (startDate <= endDate)
+            { 
+                if (startDate.DayOfWeek != DayOfWeek.Saturday && startDate.DayOfWeek != DayOfWeek.Sunday)
+                    workDays++;
+                startDate = startDate.AddDays(1);
+            }
+            Console.WriteLine($"Number of Workdays: {workDays}");
+
+        }
+
+
+
+    }
+}
