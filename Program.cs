af8a9e2e314079583c2c782004eed4fcc7deb696
Puja Borse <pujaborse13@gmail.com>
2025-02-07 13:54:30 +0530

    Calculate sum of even and odd digit in Integer

--- Program.cs
+++ Program.cs
@@ -8,35 +8,43 @@ class Program
         //Console .WriteLine("Welcome to problem solving");
         /*
 
-        //Movie Ticket Price
-        MovieTicketPrice movieTicketPrice = new MovieTicketPrice();
-        movieTicketPrice.CalculateTicketPrice();
+                    //Movie Ticket Price
+                    MovieTicketPrice movieTicketPrice = new MovieTicketPrice();
+                    movieTicketPrice.CalculateTicketPrice();
 
-        //Log In System
-        LogInSystem logInSystem = new LogInSystem();
-        logInSystem.LogIn();
-
-
-
-        //Menu Driven Calculator
-        MenuDrivenCalculator menuDrivenCalculator = new MenuDrivenCalculator();
-        menuDrivenCalculator.Calculate();
+                    //Log In System
+                    LogInSystem logInSystem = new LogInSystem();
+                    logInSystem.LogIn();
 
 
-        //Calculate Air Quality Index 
-        CalculateAirQualityIndex calculator = new CalculateAirQualityIndex();
-        calculator.CalculateAQI();
-        
-        */
-        //Display Current Time In DifferentZone
-        CurrentTimeInDifferentZone currentTimeInDifferentZone = new CurrentTimeInDifferentZone();
-        currentTimeInDifferentZone.DisplayCurrentTimeInDifferentZone();
 
-        
+                    //Menu Driven Calculator
+                    MenuDrivenCalculator menuDrivenCalculator = new MenuDrivenCalculator();
+                    menuDrivenCalculator.Calculate();
 
-        ////Number of Workdays between two dates
-        //NumberOfWorkdays numberOfWorkdays = new NumberOfWorkdays();
-        //numberOfWorkdays.CalculateWorkDays();
+
+                    //Calculate Air Quality Index 
+                    CalculateAirQualityIndex calculator = new CalculateAirQualityIndex();
+                    calculator.CalculateAQI();
+
+
+                    //Display Current Time In DifferentZone
+                    CurrentTimeInDifferentZone currentTimeInDifferentZone = new CurrentTimeInDifferentZone();
+                    currentTimeInDifferentZone.DisplayCurrentTimeInDifferentZone();
+
+
+
+                    //Number of Workdays between two dates
+                      NumberOfWorkdays numberOfWorkdays = new NumberOfWorkdays();
+                      numberOfWorkdays.CalculateWorkDays();
+
+                */
+
+
+        //calculates the sum of even and odd digits in a given integer separately.
+         SumOfEvenOddDigit CalculateSumOfEvenOddDigit = new SumOfEvenOddDigit();
+         CalculateSumOfEvenOddDigit.CalculateSumOfEvenOddDigit();
+
 
 
 
---
+++ SumOfEvenOddDigit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolvingUsingCSharp
+{
+    internal class SumOfEvenOddDigit
+    {
+
+        public void CalculateSumOfEvenOddDigit()
+        {
+            Console.Write("Enter a number:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int evenSum = 0;
+            int oddSum = 0;
+
+            while (input != 0)
+            {
+                int digit = input % 10;
+
+                if (digit % 2 == 0)
+                    evenSum += digit;
+                else
+                    oddSum += digit;
+
+                input = input / 10;
+
+            
+            }
+
+            Console.WriteLine($"Sum of Even Digits: {evenSum}");
+            Console.WriteLine($"Sum of Odd Digits: {oddSum}");
+
+        }
+    }
+}
